034e429dcf3fe2942fa2f25a6eebfa4b1fe2ca61
khyiu <superk888@gmail.com>
2021-09-18 18:10:16 +0200

    Add mapping method (expense -> expense response) + tests

--- src/main/java/be/kuritsu/hetb/mapper/ExpenseMapper.java
+++ src/main/java/be/kuritsu/hetb/mapper/ExpenseMapper.java
@@ -1,13 +1,22 @@
 package be.kuritsu.hetb.mapper;
 
+import java.util.UUID;
+
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 import be.kuritsu.het.model.ExpenseRequest;
+import be.kuritsu.het.model.ExpenseResponse;
 import be.kuritsu.hetb.domain.Expense;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface ExpenseMapper {
 
     Expense expenseRequestToRequest(ExpenseRequest expenseRequest);
+
+    ExpenseResponse expenseToExpenseResponse(Expense expense);
+
+    default String fromUUID(UUID id) {
+        return id.toString();
+    }
 }
--- src/test/java/be/kuritsu/hetb/mapper/ExpenseMapperTest.java
+++ src/test/java/be/kuritsu/hetb/mapper/ExpenseMapperTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.UUID;
 
 import org.junit.Test;
 
 import be.kuritsu.het.model.ExpenseRequest;
+import be.kuritsu.het.model.ExpenseResponse;
 import be.kuritsu.hetb.domain.Expense;
 
 public class ExpenseMapperTest {
@@ -46,5 +51,35 @@ public class ExpenseMapperTest {
         assertThat(expense.getTags())
                 .hasSize(3)
                 .contains(expenseTags);
+    }
+
+    @Test
+    public void test_expense_to_expense_response_null_input() {
+        ExpenseResponse expenseResponse = expenseMapper.expenseToExpenseResponse(null);
+        assertThat(expenseResponse).isNull();
+    }
+
+    @Test
+    public void test_expense_to_expense_response() {
+        LocalDate date = LocalDate.of(2021, 9, 17);
+        Expense expense = Expense.builder()
+                .id(UUID.randomUUID())
+                .date(date)
+                .amount(BigDecimal.valueOf(-746.22))
+                .tags(Set.of("voiture"))
+                .description("Entretien annuel - garage Beerens Zaventem")
+                .paidWithCreditCard(false)
+                .owner("test-user")
+                .order(LocalDateTime.of(date, LocalTime.ofSecondOfDay(1)))
+                .build();
+
+        ExpenseResponse expenseResponse = expenseMapper.expenseToExpenseResponse(expense);
+        assertThat(expenseResponse.getId()).isEqualTo(expense.getId().toString());
+        assertThat(expenseResponse.getDate()).isEqualTo(expense.getDate());
+        assertThat(expenseResponse.getAmount()).isEqualTo(expense.getAmount());
+        assertThat(expenseResponse.getTags()).containsExactlyElementsOf(expense.getTags());
+        assertThat(expenseResponse.getDescription()).isEqualTo(expense.getDescription());
+        assertThat(expenseResponse.getPaidWithCreditCard()).isEqualTo(expense.getPaidWithCreditCard());
+        assertThat(expenseResponse.getCreditCardStatementIssued()).isEqualTo(expense.getCreditCardStatementIssued());
     }
 }
