b4c8bf0c95b56649ad20cee4924cc0307b789da1
khyiu <superk888@gmail.com>
2021-09-18 11:39:42 +0200

    Code cleanup

--- src/main/java/be/kuritsu/hetb/domain/Expense.java
+++ src/main/java/be/kuritsu/hetb/domain/Expense.java
@@ -22,7 +22,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(schema = "het", name = "expense")
