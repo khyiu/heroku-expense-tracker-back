6da3d41106ea08fed39578740b7c23d760601980
khyiu <superk888@gmail.com>
2022-06-11 23:21:03 +0200

    HET-20 Upgrade Spring Boot to 2.7.x

--- src/main/java/be/kuritsu/hetb/config/KeycloakConfig.java
+++ src/main/java/be/kuritsu/hetb/config/KeycloakConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class KeycloakConfig {
 
     @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
 }
